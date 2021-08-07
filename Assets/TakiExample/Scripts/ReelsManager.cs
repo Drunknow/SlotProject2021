7f860bacdf8eb8107a88986ca78e27ddf5c828da
田き <prgrm2960.17chan@gmail.com>
2021-08-07 15:20:03 +0900

    revert revert

--- Assets/TakiExample/Scripts/ReelsManager.cs
+++ Assets/TakiExample/Scripts/ReelsManager.cs
@@ -11,9 +11,9 @@ namespace SlotProject.TakiExample
 
         //神からイベントを受け取るのだ。
         //具体的には、全てのリールが止まった時に発火すべき関数。
-        Action allReelStopEvent;
+        Action<int> allReelStopEvent;
         //一応プロパティ化
-        public Action AllReelStopEvent
+        public Action<int> AllReelStopEvent
         {
             get
             {
@@ -33,10 +33,12 @@ namespace SlotProject.TakiExample
         /// 自身が参照している全てのリールを回す関数
         /// </summary>
         public void StartAllReel()
-        {            
+        {
+            SlotRoleDetaminer slotRoleDetaminer = new SlotRoleDetaminer();
+            int[] hitReelSymbols = slotRoleDetaminer.DetaminAllSymbol();
             for(int i = 0; i < Reels.Length; i++)
             {
-                Reels[i].GetComponent<IReelStartable>().StartReel();
+                Reels[i].GetComponent<IReelStartable>().StartReel(hitReelSymbols[i]);
                 Reels[i].GetComponent<IReelStartable>().SetStopEvent(StopOneReel);
             }
             stopedReelCount = 0;//全てのリールは回り始める
@@ -50,20 +52,35 @@ namespace SlotProject.TakiExample
             //もし、止まっているリールの数が参照の数といっしょなら、神から受け取ったイベントを発火する。
             if(stopedReelCount == Reels.Length)
             {
-                //まずはすべてのリールの情報を取得する。
-                GetCurrentZugara();
 
                 //値段を判断する。
-                SlotRoleJudgement slotRoleJudgement = new SlotRoleJudgement();//←これ最高のネーミングではないですか？
+                SlotRoleJudgement たなべ君 = new SlotRoleJudgement();//←これ最高のネーミングではないですか？
                 //すべてのリールの情報を引数として、たなべ君(役を判定してくれる人)に渡す
-                int getCoin = slotRoleJudgement.CheckSlotReel(GetCurrentZugara());
+                int getCoin = たなべ君.CheckSlotReel(GetCurrentZugara());
+
+                /*デバッグ用
+                int[][] debugindex= GetCurrentZugara();
+                for(int i = 0; i < debugindex.Length; i++)
+                {
+                    for(int j = 0; j < debugindex[i].Length; j++)
+                    {
+                        Debug.Log(i + "番目のリールの" + j + "番目は" + debugindex[i][j]);
+                    }
+                }
+                */
 
                 //最後に受け取った関数を発火する。
-                allReelStopEvent();
+                allReelStopEvent(getCoin);
             }
         }
 
 
+        /// <summary>
+        /// すべての図柄を取得して返す。
+        /// 各リールごとに配列とし、
+        /// 二次元配列で返す。
+        /// </summary>
+        /// <returns></returns>
         public int[][] GetCurrentZugara()
         {
             int[][] reals = new int[3][];
@@ -73,10 +90,7 @@ namespace SlotProject.TakiExample
 
             for(int i = 0; i < 3; i++)
             {
-                for (int j = 0; j < 3; j++)
-                {
-                    reals[i][j] = Reels[i].GetComponent<Reel>().GetAllReel()[j];
-                }
+                reals[i] = Reels[i].GetComponent<Reel>().GetAllReel();           
             }
 
             return reals;
--- Assets/TakiExample/Scripts/SlotRoleJudgement.cs
+++ Assets/TakiExample/Scripts/SlotRoleJudgement.cs
@@ -1,18 +1,42 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace SlotProject.TakiExample
 {
     /// <summary>
-    /// ���ۂɒl�����Ăǂ�Ȗ���������Ă��邩�m�F�����ڂ����B
-    /// �����A��̓I�Ȗ���Ԃ��ƃA���Ȃ̂ŁA���Ԗڂ̐��l�����ł���������݂̂�Ԃ��Ɨǂ������H
+    /// 実際に値を見てどんな役がそろっているか確認する役目を持つ。
+    /// ただ、具体的な役を返すとアレなので、何番目の数値が何でそろったかのみを返すと良さそう？
     /// </summary>
     public class SlotRoleJudgement
     {
 
-        //��ɓ��邱����(�f�o�b�O�p)
+        //手に入るこいん(デバッグ用)
         int[] coin = { 1, 10, 100, 1000, 10000, 100000, 1000000 };
 
-   }
+
+        public int CheckSlotReel(int[][] reals)
+        {
+            
+            int totalCoin =0;
+
+            //連想配列的な発想で、めんどくさい処理をfor分に落とし込む。
+            //実際はすべてのリールを管理するクラスを作って、あヴぇくんの言うようにforeachを用いると良さそうです。
+            int[] reel1Index = {2};
+            int[] reel2Index = {2};
+            int[] reel3Index = {2};
+
+            for(int i = 0; i < reel1Index.Length; i++)
+            {
+                if(reals[0][reel1Index[i]] == reals[1][reel2Index[i]] && reals[0][reel1Index[i]] == reals[2][reel3Index[i]])
+                {
+                    totalCoin += coin[reals[0][reel1Index[i]]];
+                }
+            }
+
+            return totalCoin ;
+
+        }
+
+    }
 }
